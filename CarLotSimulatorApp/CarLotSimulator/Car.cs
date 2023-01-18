71eb63010325c875edaa9b093295405754ed5eab
Amy Galbicsek <acgalbicsek@gmail.com>
2023-01-17 17:25:52 -0900

    finished car lot

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }  
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string EngineNoise)
+        {
+            Console.WriteLine(EngineNoise);
+
+        }
+        public void MakeHonkNoise(string HonkNoise)
+        {
+            Console.WriteLine(HonkNoise);
+        }
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,22 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CarLotSimulator
 {
     class Program
-    {
+    {   
+        
+        
         static void Main(string[] args)
         {
+            var lot = new CarLot();
+            
+
+
+            Car myCar = new Car();
+            myCar.Make = "Volkswagon";
+            myCar.Model = "Corrado";
+            myCar.Year = 1988;
+            myCar.EngineNoise = "Sounds like a rabbit in distress";
+            myCar.HonkNoise = "Goose Honk";
+            myCar.IsDriveable = "Slowly but surely";
+
+            lot.Cars.Add(myCar);
+
+            var Ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Bronco",
+                Year = 1989,
+                EngineNoise = "The stampede of wild horses",
+                HonkNoise = "LOUD",
+                IsDriveable = "awww YYEAAAHHH",
+
+
+            };
+            lot.Cars.Add(Ford);
+
+            var amc = new Car()
+            {
+                Make = "AMC",
+                Model = "Eagle",
+                Year = 1981,
+                EngineNoise = "vroom vroom",
+                HonkNoise = "Look for hand gesture out the window",
+                IsDriveable = "Only driveable in Wisconsin",
+
+            };
+            lot.Cars.Add(amc);
+
+           
+            myCar.MakeEngineNoise(myCar.EngineNoise);
+            myCar.MakeHonkNoise(myCar.HonkNoise);
+            
+            Ford.MakeEngineNoise(Ford.EngineNoise);
+            Ford.MakeHonkNoise(Ford.HonkNoise);
+            
+            amc.MakeEngineNoise(amc.EngineNoise);
+            amc.MakeHonkNoise(amc.HonkNoise);
+
+
+
+            foreach(var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model} IsDriveable: {car.IsDriveable}");
+            }
+
+            
+            
+
+            
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //DONE Create a seperate class file called Car
+            //DONE Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //DONE Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //DONE The methods should take one string parameter: the respective noise property
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            // DONE Now that the Car class is created we can instanciate 3 new cars
+            // DONE Set the properties for each of the cars
+            // Call each of the methods for each car
 
             //*************BONUS*************//
 
